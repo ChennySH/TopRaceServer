ed55d51a7ee952e7b6c0d2e3ea0c27bfe73157c7
chenny shahir <chenny.shahir@gmail.com>
2022-01-19 18:15:50 +0200

    sync

--- TopRaceServer/Controllers/TopRaceController.cs
+++ TopRaceServer/Controllers/TopRaceController.cs
@@ -55,21 +55,17 @@ namespace TopRaceServer.Controllers
                 return false;
             else
             {
-                Player p = new Player()
-                {
-                   
+                user.Player = new Player()
+                {                 
                     PlayerName = user.UserName,
                     WinsNumber = 0,
                     LosesNumber = 0,
                     WinStreak = 0,
                     ProfilePic = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
                 };
-                //p.Users.Add(user);
                 
-                this.context.Players.Add(p);
+                this.context.Users.Add(user);
                
-                this.context.SaveChanges();
-                p.Users.Add(user);
                 this.context.SaveChanges();
                 return true;
             }
